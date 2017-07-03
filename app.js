ce0db1a1b3cd157c98f9235b5724573f453c407a
José Antonio Reyes <imjaroiswebdev@gmail.com>
2017-07-03 07:07:35 -0400

    Index for Jarois PC Repair implemented.
    Views will be manage through Pug templating.

--- app.js
+++ app.js
@@ -14,15 +14,20 @@ var app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+// Will use for views Pug templates
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 // Directory for static assets
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/', (req, res) => {
-	res.status(200).send({
-		message: 'Hello friend :]'
-	})
-	console.log('Hello friend :]')
+	let tplOptions = {
+		pgTitle: 'Jarois PC Repair',
+		mainTitle: 'We fix Your Computer fast and LOW PRICE'
+	}
+	res.render('index', tplOptions)
 })
 
 app.listen(port)
