030c9feeb88bf6a1446fcf1a913327ab68e2fc11
José Antonio Reyes <imjaroiswebdev@gmail.com>
2017-07-04 21:17:07 -0400

    Contact page ready and form working. I'm done here!
    The contact form has been setup with nodemailer using
    gmail SMTP.
    
    El formulario de contacto se configuró con nodemailer
    usando el SMTP de gmail.

--- app.js
+++ app.js
@@ -22,12 +22,75 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+// Index
 app.get('/', (req, res) => {
 	let tplOptions = {
 		pgTitle: 'Jarois PC Repair',
-		mainTitle: 'We fix Your Computer fast and LOW PRICE'
+		mainTitle: 'We fix Your Computer fast and at LOW PRICE'
 	}
 	res.render('index', tplOptions)
+})
+
+// Contact us
+app.get('/contact', (req, res) => {
+	let tplOptions = {
+		pgTitle: 'Jarois PC Repair|We fix your PC today',
+		mainTitle: 'Contact us and get your PC repair today'
+	}
+	res.render('contact', tplOptions)
+})
+
+// Send form
+app.post('/contact/send', (req, res) => {
+	// Sender credentials with gmail
+	let transporterOpt = {
+		service: 'Gmail',
+		auth: {
+			user: 'yourgmailuser@gmail.com',
+			pass: 'you5up3rP4ss'
+		}
+	}
+
+	const transporter = nodemailer.createTransport(transporterOpt)
+
+	let senderName = req.body.sendername,
+		senderEmail = req.body.senderemail,
+		senderPhone = req.body.senderphone,
+		senderMsg = req.body.senderMsg;
+
+	const plainText = `We have a submition from a visitor.
+		Here the details...
+		From: ${senderName}.
+		Email: ${senderEmail}.
+		Phone: ${senderPhone}.
+		PC issue: ${senderMsg}.`;
+
+	const htmlVersion = `<h3>We have a submition from a visitor.</h3>
+		<p>Here the details...</p>
+		<ul>
+			<li>From: ${senderName}.</li>
+			<li>Email: ${senderEmail}.</li>
+			<li>Phone: ${senderPhone}.</li>
+			<li>PC issue: ${senderMsg}.</li>
+		</ul>`;
+
+	let mailContent = {
+		from: 'Jarois PC Rapair visitor <do-not-answer@jaroispcrepair.com>',
+		to: 'Expris1947@jourrapide.com',
+		subject: 'Website submition',
+		text: plainText,
+		html: htmlVersion
+	}
+
+	transporter.sendMail(mailContent, (err, info) => {
+		if(err) {
+			console.log(err)
+			res.redirect('/')
+		} else {
+			console.log(`Message successfully sent: ${info.response}`)
+			res.redirect('/')	
+		}
+	})
 })
 
 app.listen(port)
